94444513c4c40acbb612eb5cc992f87178ca1b19
eu-ny <106949433+eu-ny@users.noreply.github.com>
2022-10-06 15:28:12 +0900

    20221006

--- htdocs/include/footer.php
+++
@@ -1,8 +0,0 @@
-<footer id="footer">
-        <h2>푸터영역입니다.</h2>
-        <div class="footer__inner container">
-            <address>Copyright @2022 TOMETO</address>
-            <div>blog by TOMETO</div>
-        </div>
-    </footer>
-    <!-- //footer -->
