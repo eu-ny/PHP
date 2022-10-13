d05406011c8351518de11088459929e5d98e40ec
eu-ny <106949433+eu-ny@users.noreply.github.com>
2022-10-13 14:44:31 +0900

    20221013

--- include/footer.php
+++ include/footer.php
@@ -2,7 +2,10 @@
         <h2>푸터영역입니다.</h2>
         <div class="footer__inner container">
             <address>Copyright @2022 TOMETO</address>
-            <div>blog by TOMETO</div>
+            <div>
+                blog by TOMETO
+                <a href="../admin/adminJoin.php">관리자 로그인</a>
+            </div>
         </div>
     </footer>
     <!-- //footer -->
